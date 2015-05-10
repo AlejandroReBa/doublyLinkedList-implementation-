003d7daf5065e8393b75193ab5d365c7b44942c2
= <=>
2015-05-10 22:38:24 +0200

    Primera versión de la clase de pruebas JUnit agregada.

--- src/test/java/iis/doubly/linked/list/AppTest.java
+++ src/test/java/iis/doubly/linked/list/AppTest.java
@@ -1,35 +1,283 @@
 package iis.doubly.linked.list;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 /**
- * Unit test for simple App.
+ * Created by Jose on 10/05/2015.
  */
-public class AppTest extends TestCase {
-   /**
-     * Create the test case
-     *
-     * @param testName
-     *            name of the test case
-     */
-   public AppTest(String testName) {
-       super(testName);
-  }
- 
-    /**
-     * @return the suite of tests being tested
-     * */
+public class AppTest {
 
-   public static Test suite() {
-       return new TestSuite(AppTest.class);
-   }
+    private static final int NUM_ELEMENTOS = 10;
+    private DoublyLinkedList<Integer> lista;
 
-   /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-       assertTrue(true);
-   }
+    @Test
+    public void testBefore(){
+
+        this.lista = new DoublyLinkedList<Integer>();
+
+    }
+
+    @Test
+    public void testAlCrearLaListaDebeEstaVacia(){
+
+        assertNull(this.lista);
+
+    }
+
+    @Test
+    public void testSiAgregoElementoAListaVaciaELPrimerElementoDebeSerElIntroducido(){
+
+        Integer elemento = 10;
+
+        this.lista.insertBeginning(elemento);
+
+        Integer primerElementoLista = this.lista.first();
+
+        assertEquals(elemento, primerElementoLista);
+
+    }
+
+    @Test
+    public void testSiAgregoElementoAListaVaciaElUltimoElementoDebeSerElIntroducido(){
+
+        Integer elemento = 10;
+
+        this.lista.insertBeginning(elemento);
+
+        Integer ultimoElementoLista = this.lista.last();
+
+        assertEquals(elemento, ultimoElementoLista);
+
+    }
+
+    @Test
+    public void testSiAgregoUnElementoYEliminoUnElementoAListaVaciaDebeQuedarListaVacia(){
+
+        Integer elemento = 10;
+
+        this.lista.insertBeginning(elemento);
+        this.lista.remove(elemento);
+
+        assertTrue(this.lista.isEmpty());
+        assertTrue(0 == this.lista.listSize());
+        assertTrue(this.lista.first() == null);
+        assertTrue(this.lista.last() == null);
+
+    }
+
+    @Test
+    public void testSiADosListasIgualesAgregoUnElementoIgualLasListasResultantesDebenSerIguales(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+        DoublyLinkedList<Integer> segundaLista = new DoublyLinkedList<Integer>();
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+            segundaLista.insertEnd(aleatorio);
+
+        }
+
+        assertEquals(this.lista, segundaLista);
+
+    }
+
+
+    @Test
+    public void testSiInsertoNuevoNodoAlPrincipioEsteDebeSerElPrimeroDeLaLista(){
+
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+
+        }
+
+        Integer primerElementoLista = 666666;
+
+        this.lista.insertBeginning(primerElementoLista);
+
+        assertEquals(this.lista.first(), primerElementoLista);
+
+    }
+
+    @Test
+    public void testSiInsertoNuevoNodoAlFinalEsteDebeSerElUltimoDeLaLista(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+
+        }
+
+        Integer primerElementoLista = 666666;
+
+        this.lista.insertEnd(primerElementoLista);
+
+        assertEquals(this.lista.last(), primerElementoLista);
+
+    }
+
+    @Test
+    public void testSiInsertoUnNumeroDeterminadoDeNodosAListaVaciaSuLongitudDebeSerIgualAlNumeroDeNodosIntroducidos(){
+
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+
+        }
+
+        assertEquals(this.lista.listSize(), NUM_ELEMENTOS);
+
+
+    }
+
+    @Test
+    public void testSiADosListasIgualesLesEliminoNodosDistintosLasListasResultantesSeranDistintas(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+        DoublyLinkedList<Integer> segundaLista = new DoublyLinkedList<Integer>();
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+            segundaLista.insertEnd(aleatorio);
+
+        }
+
+        this.lista.remove(this.lista.first());
+        this.lista.remove(this.lista.last());
+
+        assertNotEquals(this.lista, segundaLista);
+
+
+    }
+
+    @Test
+    public void testSiADosListarIgualesLesAgregoNodosDistintosLasListasResultantesSeranDistintas(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+        DoublyLinkedList<Integer> segundaLista = new DoublyLinkedList<Integer>();
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+            segundaLista.insertEnd(aleatorio);
+
+        }
+
+        this.lista.insertEnd(new Integer(89));
+        segundaLista.insertEnd(new Integer(90));
+
+        assertNotEquals(this.lista, segundaLista);
+
+    }
+
+    @Test
+    public void testSiAgregoUnNodoALaListaEstaDebeIncrementarSuLongitudEnUno(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+
+        }
+
+        this.lista.insertEnd(new Integer(89));
+
+
+        assertEquals(this.lista.listSize(), (NUM_ELEMENTOS + 1));
+
+    }
+
+    @Test
+    public void testSiEliminoUnNodoDeLaListaEstaDebeDecrementarEnUnoSuLongitud(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+
+        }
+
+        this.lista.remove(this.lista.first());
+
+
+        assertEquals(this.lista.listSize(), (NUM_ELEMENTOS - 1));
+
+    }
+
+    @Test
+    public void testSiEliminoTodosLosNodosDeListaEstaDebeQuedarTotalmenteVacia(){
+
+        Random generadorAletaorios = new Random();
+        Integer aleatorio;
+
+
+        for(int contador = 0; contador < NUM_ELEMENTOS; contador++){
+
+            aleatorio = generadorAletaorios.nextInt(1000);
+
+            this.lista.insertEnd(aleatorio);
+
+        }
+
+        this.lista.removeAll();
+
+
+        assertTrue(this.lista.isEmpty());
+        assertTrue(0 == this.lista.listSize());
+        assertTrue(this.lista.first() == null);
+        assertTrue(this.lista.last() == null);
+
+    }
+
+
+    @Test
+    public void testDespues(){
+
+        this.lista = null;
+
+    }
 }
