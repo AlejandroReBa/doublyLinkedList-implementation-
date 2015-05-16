38ad0e25c4f10f4e9d3358bd6050b18dd6e77c67
Alejandro Reyes Bautista <reyes.bautista.alejandro@gmail.com>
2015-05-16 11:03:47 +0200

    DoublyLinkedLlistException.java included

---
+++ src/main/java/iis/doubly/linked/list/DoublyLinkedListException.java
@@ -0,0 +1,14 @@
+package iis.doubly.linked.list;
+
+@SuppressWarnings("serial")
+public class DoublyLinkedListException extends RuntimeException {
+
+    public DoublyLinkedListException() {
+        super();
+    }
+
+    public DoublyLinkedListException(String msg) {
+        super(msg);
+    }
+
+}
